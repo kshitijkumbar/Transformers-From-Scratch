aa4c729bdcaf048196107194d5efd6ba679c6cdb
Kshitij Kumbar <kshitij.kumbar@outlook.com>
2024-03-11 21:31:15 +0000

    Add embedding module

--- Embedding.py
+++ Embedding.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import math
 class InputEmbedding(nn.Module):
 
     def __init__(self, d_model: int, vocab_size: int):
@@ -8,3 +8,7 @@ class InputEmbedding(nn.Module):
 
         self.d_model = d_model
         self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(self.vocab_size, d_model)
+    
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
