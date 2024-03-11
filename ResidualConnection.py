2e6e2457faa6d4fbd568b494c412a5863d878f64
Kshitij Kumbar <kshitij.kumbar@outlook.com>
2024-03-11 23:43:42 +0000

    Add Residual conn module

---
+++ ResidualConnection.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout_p: float) -> None:
+        super().__init__()
+
+        self.dropout = nn.Dropout(dropout_p)
+        self.norm = nn.LayerNorm(eps=1e-9)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
